7f2a3f27c79f5989f0dfadcd67aa3f57e1145a1d
TwlightOne <privateemail555po@gmail.com>
2024-07-28 08:31:54 +0700

    Update MoonLeap.lua

--- MoonLeap.lua
+++ MoonLeap.lua
@@ -1,17 +1,25 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Moon Leap", "BloodTheme")
-local Tab = Window:NewTab("LocalPlayer")
-local Section = Tab:NewSection("Character")
-local Plr = game:GetService("Players").LocalPlayer
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Moon Leap", HidePremium = false, SaveConfig = true, ConfigFolder = ""})
+local Plr = game.Players.LocalPlayer
 local Humanoid = Plr.Character:WaitForChild("Humanoid")
-Section:NewButton("Reset Fast", "Health 100 -> 0", function()
-	Humanoid.Health = 0
-end)
 
-Section:NewSlider("WalkSpeed", "WalkSpeed", 5000, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
-	Humanoid.WalkSpeed = s
-end)
+local Tab = Window:MakeTab({
+	Name = "Executor",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Charaacter"
+})
+local Section2 = Tab:AddSection({
+	Name = "Time: "..os.time()
+})
 
-Section:NewSlider("JumpPower", "JumpPower", 5500, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
-	Humanoid.JumpPower = s
-end)
+Tab:AddTextbox({
+	Name = "Script:",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		loadstring(Value)
+	end	  
+})
